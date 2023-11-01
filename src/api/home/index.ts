9302866f162da60cf418bd18fbf955879d3ec7cc
summer <2091389698@qq.com>
2023-11-01 17:51:35 +0800

    [summer]11.01结束

--- src/api/home/index.ts
+++ src/api/home/index.ts
@@ -4,8 +4,12 @@ import type {HospitalResponseData} from '@/api/home/type.ts'
 
 // 通过枚举管理首页模块的接口地址
 enum API {
-    HOSPITAL_URL = '/hosp/hospital/'
+    HOSPITAL_URL = '/hosp/hospital/',
+    HOSPITALLEVELANDREGION_URL = '/cmn/dict/findByDictCode/'
 }
 
 // 获取医院的数据
-export const reqHospital = (page: number, limit: number) => request.get<any,HospitalResponseData>(API.HOSPITAL_URL + `${page}/${limit}`);
+export const reqHospital = (page: number, limit: number) => request.get<any, HospitalResponseData>(API.HOSPITAL_URL + `${page}/${limit}`);
+
+// 获取医院的等级或者医院的地区
+export const reqHospitalLevelAndRegion = (dicCode:string)=>request.get<any>(API.HOSPITALLEVELANDREGION_URL+dicCode)
